46efbe9d41855b5d67f3a5a2f07615cb6846ab0d
Nikos Bregiannis <nikos.bre@gmail.com>
2022-01-12 10:11:29 -0800

    Add copyright

--- internal/webhook/webhook.go
+++ internal/webhook/webhook.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Cisco and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package webhook
 
 import (
@@ -96,12 +112,14 @@ func (w *MsmWebhook) Init(ctx context.Context) error {
 	return nil
 }
 
+// Start starts the webhook server
 func (w *MsmWebhook) Start() error {
 	w.Log.Infof("Server successfully started: listening on port 443")
 
 	return w.server.ListenAndServeTLS("", "")
 }
 
+// Close safely closes the server
 func (w *MsmWebhook) Close() {
 	defer w.Log.Infof("Server successfully closed")
 
